651d933266176dd61e2ef92d73995a2d91d631f5
thehien <nihongodakenb93>
2022-10-29 08:27:29 +0900

    add then ports

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import '.\inde'
-
+import 'kkkjj'
 const container = document.getElementById('root');
 const root = createRoot(container);
 
